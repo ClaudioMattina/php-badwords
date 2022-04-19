aae97b7ff5e9c1c527dcc373c6f0fe8a45bf8398
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-19 19:01:00 +0200

    fine

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
 <?php 
 
-$nome = "claudio"; 
+$paragrafo = "Salve sign."; 
 
-$cognome = " ". $_GET['cognome'] . " ";
+$cognome = " ". $_GET['lastName'] . " ";
 
-echo rtrim($nome) . rtrim($cognome);
+
+
+echo rtrim($paragrafo);
+
+$array=["ciao", "ragazzo", "ti", "chaimi", $_GET['lastName'],];
+
+
 ?>
 
 <pre>
     <?php
 
-    var_dump(strlen($nome . $cognome))
+    var_dump($array);
+    var_dump(strlen($paragrafo));
+    var_dump(str_replace($_GET['lastName'], "***",implode(" ", $array ) ) );
+    
 
     ?>
 </pre>
